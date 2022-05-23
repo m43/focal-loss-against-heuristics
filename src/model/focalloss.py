93d024b9ca06c5f805c22a102754e1da39baf1ea
Axel <axel.marmet@epfl.ch>
2022-05-23 10:11:36 +0200

    Test and correct focal loss

--- src/model/focalloss.py
+++ src/model/focalloss.py
@@ -4,12 +4,22 @@ import torch.nn.functional as F
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma: float):
+    def __init__(self, gamma: float, reduction='none'):
         super().__init__()
+
+        assert reduction in ['none', 'sum', 'mean']
+
         self.gamma = gamma
+        self.reduction = reduction
 
     def forward(self, input_logits: torch.Tensor, onehot_targets: torch.Tensor):
-        input_probs = torch.softmax(input_logits, dim=-1)
-        ce_loss = torch.nn.functional.cross_entropy(input_probs, onehot_targets, reduction='none')
+        ce_loss = torch.nn.functional.cross_entropy(input_logits, onehot_targets, reduction='none')
         input_probs_for_target = torch.exp(-ce_loss)
-        return (1 - input_probs_for_target) ** self.gamma * ce_loss
+        loss = (1 - input_probs_for_target) ** self.gamma * ce_loss
+
+        if self.reduction == 'sum':
+            loss = loss.sum(dim=-1)
+        elif self.reduction == 'mean':
+            loss = loss.mean(dim=-1)
+
+        return loss
---
+++ test/test_focalloss.py
@@ -0,0 +1,52 @@
+import unittest
+
+from src.model.focalloss import FocalLoss
+import torch
+import torch.nn.functional as F
+
+
+class TestFocalLoss(unittest.TestCase):
+
+    def test_gamma_zero_same_as_CE(self):
+        """
+        Test that when gamma is 0 we are similar to CE
+        """
+        BATCH_SIZE = 32
+        NUM_CLASSES = 5
+
+        logits = torch.randn((BATCH_SIZE, NUM_CLASSES)) * 5
+        targets = F.one_hot(
+            torch.randint(high=NUM_CLASSES, size=(BATCH_SIZE,)),
+            num_classes=NUM_CLASSES
+        ).float()
+
+        expected = F.cross_entropy(logits, targets, reduction='none')
+        actual = FocalLoss(gamma=0., reduction='none').forward(logits, targets)
+
+        self.assertTrue(
+            torch.all(torch.isclose(expected, actual)),
+        )
+
+    def test_formula_applied_correctly(self):
+
+        logits = torch.tensor([
+            [10,  9,  -5],
+            [ 0,  2, -10]
+        ]).float()
+        probs = F.softmax(logits, dim=-1).max(dim=-1)[0]
+
+        targets = torch.tensor([
+            [1, 0, 0],
+            [0, 1, 0]
+        ]).float()
+
+        for gamma in [1., 2., 5., 10.]:
+            expected = - (((1 - probs)**gamma) * torch.log(probs))
+            actual = FocalLoss(gamma=gamma, reduction='none').forward(logits, targets)
+
+            self.assertTrue(
+                torch.all(torch.isclose(expected, actual)),
+            )
+
+if __name__ == '__main__':
+    unittest.main()
