51b591dfad850da90b7568643b96294811205c6f
Axel <axel.marmet@epfl.ch>
2022-05-31 18:58:34 +0200

    Add logging of loss from different kind of samples present in MNLI

--- src/dataset/datamodule.py
+++ src/dataset/datamodule.py
@@ -6,7 +6,7 @@ from pytorch_lightning.utilities.cli import DATAMODULE_REGISTRY
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, PreTrainedTokenizerBase, DataCollatorWithPadding
 
-from src.constants import HEURISTIC_TO_INTEGER
+from src.constants import HEURISTIC_TO_INTEGER, SampleType
 from src.model.nlitransformer import PRETRAINED_MODEL_ID
 from src.utils.util import get_logger
 
@@ -48,6 +48,21 @@ class ExperimentDataModule(pl.LightningDataModule):
             res['heuristic'] = [HEURISTIC_TO_INTEGER[sample] for sample in batch['heuristic']]
             return res
 
+        def process_mnli(sample):
+            sample_type = SampleType.STANDARD
+            if sample['premise'] == sample['hypothesis']:
+                sample_type = SampleType.TRIVIAL if sample['label'] == 0 else SampleType.NOISE
+
+            tokenized_premise = self.tokenizer(sample['premise'], add_special_tokens=False)['input_ids']
+            tokenized_hypothesis = self.tokenizer(sample['hypothesis'], add_special_tokens=False)['input_ids']
+            if all(token in tokenized_premise for token in tokenized_hypothesis):
+                sample_type = SampleType.HEURISTIC_E if sample['label'] == 0 else SampleType.HEURISTIC_NE
+
+            new_attributes = {'type': sample_type.value}
+            new_attributes.update(self.tokenizer(sample['premise'], sample['hypothesis']))
+
+            return new_attributes
+
         self.hans_dataset = load_dataset("hans", split='validation').map(
             tokenize_hans,
             batched=True,
@@ -59,16 +74,10 @@ class ExperimentDataModule(pl.LightningDataModule):
         )
         log.info(f"Hans validation dataset loaded, datapoints: {len(self.hans_dataset)}")
 
-        self.mnli_dataset = load_dataset("multi_nli").map(
-            lambda batch: self.tokenizer(
-                batch['premise'],
-                batch['hypothesis'],
-            ),
-            batched=True,
-            batch_size=self.batch_size)
+        self.mnli_dataset = load_dataset("multi_nli").map(process_mnli)
         self.mnli_dataset.set_format(
             type='torch',
-            columns=['input_ids', 'token_type_ids', 'attention_mask', 'label']
+            columns=['input_ids', 'token_type_ids', 'attention_mask', 'label', 'type']
         )
         log.info(f"MNLI dataset splits loaded:")
         log.info(f"   len(self.mnli_dataset['train'])={len(self.mnli_dataset['train'])}")
